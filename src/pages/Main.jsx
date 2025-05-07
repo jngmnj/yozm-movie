2c29536cbd19f34e540f8b0ad71eefe89a8457e2
jngmnj <exorbitance@naver.com>
2025-05-07 15:08:07 +0900

    ✨ Feat: TMDB로 메인페이지 렌더링
    - 메인페이지 더미데이터아닌 TMDB 데이터 요청하여 받은것 출력

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,38 +1,23 @@
 import Inner from "@components/common/Inner";
 import MainBanner from "@components/main/MainBanner";
 import MovieList from "@components/movie/MovieList";
-import data from "@data/movieListData.json";
-import { useState } from "react";
+import { useFetch } from "@hooks/useFetch";
+import { getPopularMovies, options } from '../utils/getMovies';
 
 const Main = () => {
-  const [movies, setMovies] = useState(data.results);
+  const { data, error, loading } = useFetch({ options, query: getPopularMovies });
 
-  if (!movies) return null;
-  const today = new Date();
+  if (!data) return null;
 
-  const mainBannerDatas = movies
+  const nonAdultMovies = data.results.filter((movie) => !movie.adult);
+  
+  const mainBannerDatas = nonAdultMovies
     .sort((a, b) => b.popularity - a.popularity)
     .slice(0, 10);
 
-  const popularMovies = movies
+  const popularMovies = nonAdultMovies
     .sort((a, b) => b.popularity - a.popularity)
     .slice(10, 16);
-
-  const topRatedMovies = movies
-    .sort((a, b) => b.vote_average - a.vote_average)
-    .slice(0, 6);
-
-  const sortedMovies = movies.sort(
-    (a, b) => new Date(a.release_date) - new Date(b.release_date)
-  );
-
-  const upcomingMovies = sortedMovies
-    .filter((movie) => movie.status === "upComing??")
-    .slice(0, 6);
-
-  const nowPlayingMovies = sortedMovies
-    .filter((movie) => new Date(movie.release_date) <= today)
-    .slice(0, 6);
 
   return (
     <>
@@ -41,9 +26,9 @@ const Main = () => {
       <Inner className="px-4">
         {/* movieList */}
         <MovieList title={"인기순"} movies={popularMovies} />
-        <MovieList title={"별점순"} movies={topRatedMovies} />
+        {/* <MovieList title={"별점순"} movies={topRatedMovies} />
         <MovieList title={"현재상영중인 영화"} movies={nowPlayingMovies} />
-        <MovieList title={"개봉예정 영화"} movies={upcomingMovies} />
+        <MovieList title={"개봉예정 영화"} movies={upcomingMovies} /> */}
       </Inner>
     </>
   );
