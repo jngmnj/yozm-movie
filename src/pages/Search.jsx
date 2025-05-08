7c17d255b1b06fb1515953db9d114052a3453884
jngmnj <exorbitance@naver.com>
2025-05-08 18:07:56 +0900

    ✨ Refactor: Search페이지 useFetch 리팩토링
    - useFetch사용하여 리팩토링

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,55 +1,24 @@
 import Inner from "@components/common/Inner";
-import LoadingSpinner from "@components/common/LoadingSpinner";
 import MovieList from "@components/movie/MovieList";
-import { TMDB_BASE_URL } from "@constants/index";
-import { options } from "@utils/getMovies";
-import { useEffect, useState } from "react";
+import { SEARCH_MOVIES_URL } from "@constants/index";
+import { useFetch } from "@hooks/useFetch";
+import { getMovies, options } from "@utils/getMovies";
+import { useCallback } from "react";
 import { useSearchParams } from "react-router";
 
 const Search = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const query = searchParams.get("query");
+  const searchQuery = searchParams.get("query");
 
-  const [searchData, setSearchData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const url = `${SEARCH_MOVIES_URL}${searchQuery}`;
 
-  useEffect(() => {
-    if (query) {
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-          const result = await fetch(
-            `${TMDB_BASE_URL}/search/movie?query=${query}`,
-            options
-          );
-          if (!result.ok) {
-            throw new Error("Network response was not ok");
-          }
-          const data = await result.json();
-          setSearchData(data.results);
-        } catch (error) {
-          setError(error);
-        } finally {
-          setLoading(false);
-        }
-      };
+  const query = useCallback(() => getMovies(url, options), [url]);
+  const { data, loading } = useFetch({ query });
 
-      fetchData();
-    }
-  }, [searchParams, query]);
-
-  const filteredMovies = searchData.filter((movie) => !movie.adult);
   return (
     <Inner className="px-4">
-      {/* movieList */}
-      {loading && <LoadingSpinner />}
-      {!loading && filteredMovies.length === 0 && (
-        <div className="text-center text-gray-500">검색 결과가 없습니다.</div>
-      )}
-      {!loading && filteredMovies.length > 0 && (
-        <MovieList title={"검색결과"} movies={filteredMovies} />
-      )}
+      {loading && <div>Loading...</div>}
+      {!loading && data && <MovieList title={null} movies={data.results} />}
     </Inner>
   );
 };
