b90c60b6219b7b1b081e58d54fea7abdd0edeb86
jngmnj <exorbitance@naver.com>
2025-05-09 18:03:11 +0900

    ✨ Feat: App에서 setUser, main Supabase provider 연결, Header 로그인/로그아웃 상태,기능  구현
    - App.jsx: 간편(소셜)로그인시 dispatch 안됨 -> App에서 user상태 업데이트 dispatch
    - main.jsx: supabaseProvider 연결
    - Header.jsx
      - 로그인시 회원정보(프로필사진, 이름) 표시하고 드롭다운으로 마이페이지/로그아웃 버튼 UI 보여지기
      - 로그아웃 버튼 누르면 supabase의 logout과 user전역상태 logout
      - theme on/off 버튼 추가(전역상태관리)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,44 @@
 import Inner from "@components/common/Inner";
 import SearchOverlay from "@components/common/SearchOverlay";
 import Fade from "@components/modal/Fade";
+import { logout } from "@store/slice/userSlice";
+import { useAuth } from "@supabaseJS/auth/useAuth";
 import { useEffect, useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoSearch } from "react-icons/io5";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 import { RiCloseLargeLine } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
+import { useSupabaseAuth } from '../../../../../Downloads/supabase/auth/index';
 
 const Header = () => {
   const { pathname } = useLocation();
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
 
+  const dispatch = useDispatch();
+  const { theme } = useSelector((state) => state.theme);
+  const { user } = useSelector((state) => state.user);
+  const { logout: supabaseLogout } = useAuth();
+  const { getUserInfo } = useSupabaseAuth();
+    
   const handleClick = () => {
     setIsSearchOpen((prev) => !prev);
   };
 
   const handleSideClick = () => {
     setIsSideMenuOpen((prev) => !prev);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await supabaseLogout();
+      dispatch(logout());
+    } catch (error) {
+      console.error("Error during logout:", error);
+      alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
+    }
   };
 
   useEffect(() => {
@@ -41,19 +62,56 @@ const Header = () => {
           >
             <IoSearch />
           </button>
+
+          <button
+            onClick={() => dispatch({ type: "TOGGLE_THEME" })}
+            className="text-2xl text-gray-300 cursor-pointer"
+          >
+            {theme === "dark" ? <MdOutlineLightMode /> : <MdOutlineDarkMode />}
+          </button>
           <div className="items-center gap-4 hidden md:flex">
-            <Link
-              to="/login"
-              className="px-4 py-2 rounded-xl bg-green-400 hover:bg-green-500 transition text-white font-bold"
-            >
-              로그인
-            </Link>
-            <Link
-              to="/join"
-              className="px-2 py-2 rounded-xl bg-transparent hover:text-green-400 transition"
-            >
-              회원가입
-            </Link>
+            {user ? (
+              <div className="group relative">
+                {/* dropdown */}
+                <div className="rounded-full w-10 h-10 transition text-white font-bold">
+                  <img
+                    src={
+                      user?.user?.profileImageUrl ||
+                      "assets/images/common/default_profile.png"
+                    }
+                    alt="profile"
+                    className="w-10 h-10 rounded-full"
+                  />
+                </div>
+                <div className="absolute min-w-max flex-col gap-2 top-10 right-0 bg-white shadow-lg rounded-lg p-4 hidden group-hover:flex">
+                  <div>{user?.user?.userName}님</div>
+                  <Link to="/mypage" className="block transition hover:text-green-400">
+                    마이페이지
+                  </Link>
+                  <button
+                    onClick={handleLogout}
+                    className="rounded-xl bg-transparent hover:text-green-400 transition"
+                  >
+                    로그아웃
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <>
+                <Link
+                  to="/login"
+                  className="px-4 py-2 rounded-xl bg-green-400 hover:bg-green-500 transition text-white font-bold"
+                >
+                  로그인
+                </Link>
+                <Link
+                  to="/signup"
+                  className="px-2 py-2 rounded-xl bg-transparent hover:text-green-400 transition"
+                >
+                  회원가입
+                </Link>
+              </>
+            )}
           </div>
           <button
             onClick={handleSideClick}
@@ -78,20 +136,55 @@ const Header = () => {
           }`}
         >
           <div className="flex items-center gap-4 mt-4">
-            <Link
-              to="/login"
-              className="px-4 py-2 rounded-xl bg-green-400 hover:bg-green-500 transition text-white font-bold"
-            >
-              로그인
-            </Link>
-            <Link
-              to="/join"
-              className="px-2 py-2 rounded-xl bg-transparent hover:text-green-400 transition"
-            >
-              회원가입
-            </Link>
+            {user ? (
+              <div className="relative">
+                {/* dropdown */}
+                <div className="rounded-full w-10 h-10 transition text-white font-bold">
+                  <img
+                    src={
+                      user?.user.profileImageUrl ||
+                      "assets/images/common/default_profile.png"
+                    }
+                    alt="profile"
+                    className="w-10 h-10 rounded-full"
+                  />
+                </div>
+                <div className=" p-4">
+                  <Link
+                    to="/mypage"
+                    className="hover:text-green-400 transition"
+                  >
+                    마이페이지
+                  </Link>
+                  <button
+                    onClick={handleLogout}
+                    className="px-2 py-2 rounded-xl hover:text-green-400 transition cursor-pointer"
+                  >
+                    로그아웃
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <>
+                <Link
+                  to="/login"
+                  className="px-4 py-2 rounded-xl bg-green-400 hover:bg-green-500 transition text-white font-bold"
+                >
+                  로그인
+                </Link>
+                <Link
+                  to="/signup"
+                  className="px-2 py-2 rounded-xl bg-transparent hover:text-green-400 transition"
+                >
+                  회원가입
+                </Link>
+              </>
+            )}
           </div>
-          <button onClick={handleSideClick}>
+          <button
+            className="absolute top-4 right-4 text-gray-500 cursor-pointer p-2"
+            onClick={handleSideClick}
+          >
             <RiCloseLargeLine />
           </button>
         </div>
